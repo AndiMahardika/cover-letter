acdcb56d487228b8bc46b836e793c22f296fa504
AndiMahardika <andi.mahardika.mulya@gmail.com>
2025-06-16 05:31:10 +0700

    init repo

---
+++ src/app/test-page/page.tsx
@@ -0,0 +1,13 @@
+import { Button } from '@/components/ui/button'
+import React from 'react'
+
+type Props = {}
+
+export default function page({}: Props) {
+  return (
+    <>
+      <h1>Test</h1>
+      <Button className='' variant={'outline'}>Test</Button>
+    </>
+  )
+}
